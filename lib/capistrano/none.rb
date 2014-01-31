9948f706908d16582ed3ad9a65c27a10eaca74f7
Michael Bevz <myb@appfellas.co>
2014-01-31 16:44:50 +0100

    Added scm:none strategy

---
+++ lib/capistrano/none.rb
@@ -0,0 +1,42 @@
+load File.expand_path("../tasks/svn.rake", __FILE__)
+
+require 'capistrano/scm'
+
+  # Base class for SCM strategy providers.
+  #
+  # @abstract
+  #
+  # @attr_reader [Rake] context
+  #
+  # @author Hartog de Mik
+  #
+  
+  class Capistrano::None < Capistrano::SCM
+  
+      # execute hg in context with arguments
+      def svn(*args)
+        args.unshift(:svn)
+        context.execute *args
+      end
+  
+      module DefaultStrategy
+        def test
+          true
+        end
+    
+        def check
+          true
+        end
+    
+        def clone
+          #svn :checkout, repo_url, repo_path
+        end
+    
+        def update
+          #svn :update
+        end
+    
+        def release
+          #svn :export, '.', release_path
+        end
+  end
